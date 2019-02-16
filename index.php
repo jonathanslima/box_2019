f3c6e61ffd89aa08c7f301a59b027e5a79021d22
jonathanslima <jonathan633.jl@gmail.com>
2019-02-16 06:05:27 -0300

    add links to projects

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Projects 2019</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link href="https://fonts.googleapis.com/css?family=Luckiest+Guy|Roboto" rel="stylesheet">
+
+	<style>
+		body{font-family: 'Roboto', sans-serif;}
+		.title{font-family: 'Luckiest Guy', cursive;}
+	</style>
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="section links">
+				<div class="col-xs-12 text-center">
+					<h1 class="title">Front End Projects 2019</h1>
+				</div>
+				<ul class="list nav nav-pills col-xs-12">
+					<li><a href="rock_paper_scissor/rock_paper_scissor.html">Rock, Paper or Scissor <span class="active">HTML, JS, CSS</span></a></li>
+					<li><a href="relogio_hex/relogio_analogico.html">Relógio Hex Analógico<span class="active">HTML, JS, CSS</span></a></li>
+					<li><a href="relogio_hex/relogio_digital.html">Relógio Hex Digital<span class="active">HTML, JS, CSS</span></a></li>
+					<li><a href="velocicriptor/criptor.html">Criptografia<span class="active">HTML, JS, CSS</span></a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
